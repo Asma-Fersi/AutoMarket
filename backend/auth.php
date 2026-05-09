c638c345c888a9238408750411bf7d7e6969d5df
Midou <notmidouysf2003@gmail.com>
2026-05-09 01:18:59 +0100

    fix cors headers in auth.php

--- backend/auth.php
+++ backend/auth.php
@@ -1,6 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { exit(0); }
 
 require_once "db.php";
 
